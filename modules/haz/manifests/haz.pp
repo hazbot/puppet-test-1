c2c377b25339aacacf0587e017209379a95266ad
hazbot <f0h3a-github@yahoo.com>
2013-04-01 19:07:24 +0100

    done

--- modules/haz/manifests/haz.pp
+++ modules/haz/manifests/haz.pp
@@ -5,7 +5,15 @@ class haz {
 		uid => 1000,
 		comment => 'Hazard',
 		groups => ['root','wheel',] ,
+		home => '/home/hazard',
 		shell => '/bin/bash',
 		password => '$6$1nULn8FQpWFmcm3Y$tVK/sslwY3L.Hfx86Qi2dMUcE3eCxUOGjsVG6fxAH/dBXUifnQDoxUt/9b2vDb3AtTd9qiPBjMbiycMwHHyYv.',
+	}
+
+	file {'/home/hazard':
+		ensure => directory,
+		mode => 700,
+		owner => hazard,
+		group => users,
 	}
 }
